74c7f342136a4f8e7e8a507194a1464b60c278ef
Andó Attila <andoattila698@gmail.com>
2023-10-19 16:21:40 +0200

    tr

---
+++ resources/views/kutya.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.master")
+
+@section("title","Kutya")
+@section("content")
+<h1>Kutya oldal</h1>
+@endsection
---
+++ resources/views/macska.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.master")
+
+@section("title","macska")
+@section("content")
+<h1>Macska oldal</h1>
+@endsection
